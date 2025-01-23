27e742f2553be7d9a730b103894ccd56baa75803
Eric Lunt <eric@lunt.software>
2025-01-23 10:00:41 -0600

    Problem 8

---
+++ problem008/solution_test.go
@@ -0,0 +1,24 @@
+// Project Euler
+package main
+
+import "testing"
+
+func Test_solve(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"example", args{4}, 5832},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := solve(tt.args.n); got != tt.want {
+				t.Errorf("solve() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

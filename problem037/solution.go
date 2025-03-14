8940bf1405f1b166328ff57c14858a85d09fea88
Eric Lunt <eric@lunt.software>
2025-03-14 11:18:12 -0500

    Problem 37 brute force

--- problem037/solution.go
+++ problem037/solution.go
@@ -8,7 +8,7 @@ import (
 )
 
 // Problem 37: Truncatable Primes
-// Solution:
+// Solution: 748317
 func main() {
 	fmt.Println("Problem 37:", solve())
 }
@@ -17,38 +17,21 @@ func solve() int {
 	var found int
 	var sum int
 outer:
-	for i := 37; found < 11; i += 2 {
-		// If it ends in 5 or 1, not gonna work
-		if i%10 == 5 || i%10 == 1 {
-			continue
-		}
-		// Now look every other digit
-		var pow = 10
-		for pow < i {
-			n := i / pow
-			switch n % 10 {
-			case 0, 2, 4, 6, 8:
-				i = ((i+pow)/10)*10 + 1
-				continue outer
-			case 5:
-				i = ((i+pow*2)/10)*10 + 1
-				continue outer
-			}
-			if n == 1 || !common.IsPrime(n) {
-				continue outer
-			}
-			pow *= 10
-		}
-		// Now go the other way
-		for pow > 10 {
-			pow /= 10
-			if !common.IsPrime(i % pow) {
-				continue outer
-			}
-		}
+	for i := 23; found < 11; i += 2 {
 		if common.IsPrime(i) {
+			var pow = 10
+			for {
+				right := i / pow
+				if right == 0 {
+					break
+				}
+				left := i % pow
+				if left == 1 || right == 1 || !common.IsPrime(left) || !common.IsPrime(right) {
+					continue outer
+				}
+				pow *= 10
+			}
 			found++
-			fmt.Println(found, i)
 			sum += i
 		}
 	}

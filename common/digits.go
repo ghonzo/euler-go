8090c76f0050de5f3f72af4c6ca6cd7c109eae0e
Eric Lunt <eric@lunt.software>
2025-12-03 23:42:03 -0600

    Add support for big.Int to Digits

--- common/digits.go
+++ common/digits.go
@@ -1,6 +1,8 @@
 // Project Euler
 package common
 
+import "math/big"
+
 type Digits []int
 
 // DigitsFromInt returns a Digits type from a positive integer
@@ -16,10 +18,37 @@ func DigitsFromInt(n int) Digits {
 	return d
 }
 
+// DigitsFromBigInt returns a Digits type from a positive big.Int
+func DigitsFromBigInt(n *big.Int) Digits {
+	if n.Sign() == 0 {
+		return Digits([]int{0})
+	}
+	var d []int
+	ten := big.NewInt(10)
+	for n.Sign() > 0 {
+		var mod *big.Int
+		n.DivMod(n, ten, mod)
+		d = append([]int{int(mod.Int64())}, d...)
+	}
+	return d
+}
+
+// Int converts a Digits type back to an integer
 func (d Digits) Int() int {
 	var v int
 	for _, x := range d {
 		v = v*10 + x
+	}
+	return v
+}
+
+// BigInt converts a Digits type back to a big.Int
+func (d Digits) BigInt() *big.Int {
+	v := big.NewInt(0)
+	ten := big.NewInt(10)
+	for _, x := range d {
+		v.Mul(v, ten)
+		v.Add(v, big.NewInt(int64(x)))
 	}
 	return v
 }

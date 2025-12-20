1787839620a892bb3eff315ded836851cacdf24a
Eric Lunt <eric@lunt.software>
2025-12-20 15:38:08 -0600

    Problem 73

---
+++ problem073/solution.go
@@ -0,0 +1,29 @@
+// Project Euler
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/ghonzo/euler-go/common"
+)
+
+// Problem 73: Counting Fractions in a Range
+// Solution:
+func main() {
+	start := time.Now()
+	fmt.Printf("Problem 73: %d (%s)", solve(12000), time.Since(start))
+}
+
+func solve(maxN int) int {
+	var count int
+	for denom := 5; denom <= maxN; denom++ {
+		inc := denom % 2
+		for num := (denom / 3) + 1; num < (denom/2)+inc; num++ {
+			if common.GCD(num, denom) == 1 {
+				count++
+			}
+		}
+	}
+	return count
+}

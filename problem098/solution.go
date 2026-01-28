c83962a1de10136c69d6a67e26385d6dd3e00921
Eric Lunt <eric@lunt.software>
2026-01-28 11:31:48 -0600

    Problem 98

---
+++ problem098/solution.go
@@ -0,0 +1,154 @@
+// Project Euler
+package main
+
+import (
+	"fmt"
+	"os"
+	"slices"
+	"strings"
+	"time"
+
+	mapset "github.com/deckarep/golang-set/v2"
+	"github.com/etnz/permute"
+	"github.com/ghonzo/euler-go/common"
+)
+
+// Problem 98: Anagramic Squares
+// Solution: 18769 (BOARD, BROAD)
+func main() {
+	start := time.Now()
+	fmt.Printf("Problem 98: %d (%s)", solve(), time.Since(start))
+}
+
+func solve() int {
+	b, err := os.ReadFile("0098_words.txt")
+	if err != nil {
+		panic(err)
+	}
+	anagrams := make(map[string][]string)
+	for w := range strings.SplitSeq(string(b), ",") {
+		w = strings.Trim(w, "\"")
+		sorted := sortString(w)
+		anagrams[sorted] = append(anagrams[sorted], w)
+	}
+	// Maybe sort by anagram length descending?
+	var anagramRootByLength []string
+	for root, words := range anagrams {
+		if len(words) < 2 {
+			continue
+		}
+		// I'm going to make a huge assumption that no repeated letters
+		if hasRepeatedLetters(root) || len(root) > 10 {
+			continue
+		}
+		anagramRootByLength = append(anagramRootByLength, root)
+	}
+	// Now reverse sort by length
+	slices.SortFunc(anagramRootByLength, func(a, b string) int {
+		return len(b) - len(a)
+	})
+	// Now find all the squares by length
+	initializeSquaresByLength()
+	var maxSquare, maxSquareLength int
+	for _, root := range anagramRootByLength {
+		length := len(root)
+		if length < maxSquareLength {
+			// No need to check smaller lengths
+			break
+		}
+		// Check all combinations of anagrams
+		for pair := range permute.Combinations(2, anagrams[root]) {
+			ms := findMaxSquareForAnagramPair(pair[0], pair[1])
+			if ms > maxSquare {
+				maxSquare = ms
+				maxSquareLength = length
+			}
+		}
+	}	
+	return maxSquare
+}
+
+func sortString(s string) string {
+	b := []byte(s)
+	slices.Sort(b)
+	return string(b)
+}
+
+func hasRepeatedLetters(s string) bool {
+	seen := make(map[rune]bool)
+	for _, r := range s {
+		if seen[r] {
+			return true
+		}
+		seen[r] = true
+	}
+	return false
+}
+
+var squaresByLength map[int]mapset.Set[int]
+
+func initializeSquaresByLength() {
+	squaresByLength = make(map[int]mapset.Set[int])
+	for n := 4; ; n++ {
+		nsq := n*n
+		nsqDigits := common.DigitsFromInt(nsq)
+		length := len(nsqDigits)
+		if length > 10 {
+			break
+		}
+		if hasRepeatedDigits(nsqDigits) {
+			continue
+		}
+		set, ok := squaresByLength[length]
+		if !ok {
+			set = mapset.NewSet[int]()
+			squaresByLength[length] = set
+		}
+		set.Add(nsq)
+	}
+}
+
+func hasRepeatedDigits(d common.Digits) bool {
+	seen := make(map[int]bool)
+	for _, x := range d {
+		if seen[x] {
+			return true
+		}
+		seen[x] = true
+	}
+	return false
+}
+
+func findMaxSquareForAnagramPair(a, b string) int {
+	var maxSquare int
+	length := len(a)
+	candidateSquares, ok := squaresByLength[length]
+	if !ok || candidateSquares.Cardinality() < 2 {
+		return 0
+	}
+	// Try each sequence of digits
+	candidateSquares.Each(func(sq int) bool {
+		d := common.DigitsFromInt(sq)
+		mapping := make(map[rune]int)
+		for i, r := range a {
+			mapping[r] = d[i]
+		}
+		// Now find the value for b
+		var bDigits common.Digits
+		for _, r := range b {
+			bDigits = append(bDigits, mapping[r])
+		}
+		// Leading zero check
+		if bDigits[0] == 0 {
+			// keep going
+			return false
+		}
+		bInt := bDigits.Int()
+		if candidateSquares.Contains(bInt) {
+			fmt.Println(a, b, maxSquare, sq, bInt)
+			maxSquare = max(maxSquare, sq, bInt)
+		}
+		return false
+	})
+	return maxSquare
+}
